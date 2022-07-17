460acab171739a12323ab744f9679363fcd9de9a
Seb <s.luchini@cashmag.fr>
2022-07-17 11:50:56 +0200

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'cashmag-docs',
   tagline: 'Dinosaurs are cool',
-  url: 'https://cm-support2-org.github.io/support2/',
-  baseUrl: '/',
+  url: 'https://cm-support2-org.github.io/docs',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment configs.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'cm-support2-org', // Usually your GitHub org/user name.
-  projectName: 'support2', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
