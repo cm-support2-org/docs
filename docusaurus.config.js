caf6ddeafe9b327bebf7f55cdb46aa30cd9eaade
slu83 <s.luchini@cashmag.fr>
2022-07-18 09:51:27 +0200

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,7 +85,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: 'intro',
+                to: '/docs/intro',
               },
             ],
           },
