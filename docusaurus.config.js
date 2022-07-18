74979f7927f39b89b62a743d6316e079033cd2fe
slu83 <s.luchini@cashmag.fr>
2022-07-18 09:32:36 +0200

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'Dinosaurs are cool',
   url: 'https://cm-support2-org.github.io',
   baseUrl: '/docs/',
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
@@ -22,10 +22,10 @@ const config = {
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en']
-  },
+  //i18n: {
+  //  defaultLocale: 'en',
+  //  locales: ['en']
+  //},
 
   presets: [
     [
